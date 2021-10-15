069539250efd2fbc41d4679716cf5c5709bc4bcc
Amber Barksdale <barksdaleaz@gmail.com>
2021-10-14 23:17:56 -0400

    ran coverage and made a change to travis file

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
+# from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+# from django.template import loader
 from django.views import generic
 from django.urls import reverse
 from .models import Choice, Question
